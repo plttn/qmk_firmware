79666c6f04843b876d65edeaf00d01b9f4cf836c
Jack Platten <jack@platten.me>
2023-06-24 21:12:23 -0700

    Add a new One Shot Layer in Moonlander Game Mode

--- keyboards/moonlander/keymaps/plttn/keymap.c
+++ keyboards/moonlander/keymaps/plttn/keymap.c
@@ -78,7 +78,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    _______,            _______,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
         KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_HYPR,            TG(GAME),   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-        KC_LCTL,_______,_______,KC_LEFT, KC_RGHT,  _______,                       _______,             KC_UP,   KC_DOWN, KC_LBRC, KC_RBRC, MO(SYMB),
+        KC_LCTL,_______,_______,KC_LEFT, KC_RGHT,  _______,                       OSL(GAME_FUNC),          KC_UP,   KC_DOWN, KC_LBRC, KC_RBRC, MO(SYMB),
                                             KC_SPC,  KC_LALT, _______,            KC_BSPC,  KC_TAB,  KC_ENT
     ),
     [GAME_FUNC] = LAYOUT_moonlander(
